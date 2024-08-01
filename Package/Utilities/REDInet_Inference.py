209f327e9ac676d247ef5b83b0282da0773e405c
PietroMazzacuva <107180588+PietroMazzacuva@users.noreply.github.com>
2024-08-02 00:42:04 +0200

    Update REDInet_Inference.py

--- Package/Utilities/REDInet_Inference.py
+++ Package/Utilities/REDInet_Inference.py
@@ -1,23 +1,3 @@
-# Copyright (c) 2023-2024 Pietro Luca Mazzacuva <pietroluca.mazzacuva@unicampus.it>
-#
-# Permission is hereby granted, free of charge, to any person obtaining a copy
-# of this software and associated documentation files (the "Software"), to deal
-# in the Software without restriction, including without limitation the rights
-# to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
-# copies of the Software, and to permit persons to whom the Software is
-# furnished to do so, subject to the following conditions:
-#
-# The above copyright notice and this permission notice shall be included in
-# all copies or substantial portions of the Software.
-#
-# THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
-# IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
-# FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
-# AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
-# LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
-# OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
-# THE SOFTWARE.
-
 import argparse
 import glob, multiprocessing, os, time
 from datetime import datetime
@@ -163,38 +143,37 @@ class inference():
                     missings = [i  for i in range(start, stop+1, 1) if i not in srr_interval.iloc[:, 1].tolist()]
 
                     temp = []
-                    if self.Imputations == "yes" and n_missings > 0 and n_missings <=self.Max_missings:
-
-                        if self.Strandness == "yes":
-                            report += "\tWarning!!! Missing nucleotides cannot be imputed with unstranded RNAseq.\nThe execution will continue without imputations.\n"
-                            
-                        else:
-                            freqs_impud = {"A":"[1,0,0,0]", "C":"[0,1,0,0]", "G":"[0,0,1,0]", "T":"[0,0,0,1]"}
-                            complement = {"A":"T", "C":"G", "G":"C", "T":"A"}
-    
-                            if strand != 0:
-    
-                                for position in missings:
-                                    base = genome.fetch(site.Region, int(position)-1, int(position))
-                                    value = freqs_impud.get(base)
-                                    if value:
-                                        temp.append([site.Region, position, base, strand, value])
-                                    else:
-                                        pass
+                    if self.Strandness != "yes":
+                        
+                        if self.Imputations == "yes" and n_missings > 0 and n_missings <=self.Max_missings:
     
                             else:
-    
-                                for position in missings:
-                                    base = genome.fetch(site.Region, int(position)-1, int(position))
-                                    complement_base = complement.get(base)
-                                    if complement_base:
-                                        value = freqs_impud.get(complement_base)
+                                freqs_impud = {"A":"[1,0,0,0]", "C":"[0,1,0,0]", "G":"[0,0,1,0]", "T":"[0,0,0,1]"}
+                                complement = {"A":"T", "C":"G", "G":"C", "T":"A"}
+        
+                                if strand != 0:
+        
+                                    for position in missings:
+                                        base = genome.fetch(site.Region, int(position)-1, int(position))
+                                        value = freqs_impud.get(base)
                                         if value:
-                                            temp.append([site.Region, position, complement_base, strand, value])
+                                            temp.append([site.Region, position, base, strand, value])
                                         else:
                                             pass
-                                    else:
-                                        pass
+        
+                                else:
+        
+                                    for position in missings:
+                                        base = genome.fetch(site.Region, int(position)-1, int(position))
+                                        complement_base = complement.get(base)
+                                        if complement_base:
+                                            value = freqs_impud.get(complement_base)
+                                            if value:
+                                                temp.append([site.Region, position, complement_base, strand, value])
+                                            else:
+                                                pass
+                                        else:
+                                            pass
 
                     if len(temp) > 0 :
 
